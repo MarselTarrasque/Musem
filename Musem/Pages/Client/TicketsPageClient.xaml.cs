ab336d252a9f972fc1d79727ef1ff59f610fc740
Марсель <kabaler2006@gmail.com>
2024-11-29 03:41:33 +0300

    Добавьте файлы проекта.

---
+++ Musem/Pages/Client/TicketsPageClient.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Musem.Pages.Client
+{
+    /// <summary>
+    /// Логика взаимодействия для TicketsPageClient.xaml
+    /// </summary>
+    public partial class TicketsPageClient : Page
+    {
+        public class Ticket
+        {
+            public int Id { get; set; }
+            public string ExhibitionTitle { get; set; }
+            public DateTime Date { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        int _id;
+        string connectionString = @"Data Source=(localdb)\Marsel;Initial Catalog=Музей;Integrated Security=True"; // Укажите вашу строку подключения
+
+        public TicketsPageClient(int id)
+        {
+            InitializeComponent();
+            _id = id;
+            var tickets = GetTicketsForUser(_id);
+            ReportsListView.ItemsSource = tickets;
+        }
+
+        private List<Ticket> GetTicketsForUser(int userId)
+        {
+            var tickets = new List<Ticket>();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT t.Id_Ticket, e.Title AS ExhibitionTitle, t.PurchaseDate, t.Price " +
+                               "FROM Tickets t " +
+                               "JOIN Exhibitions e ON t.Id_Exhibition = e.Id_Exhibition " +
+                               "WHERE t.Id_User = @UserId";
+
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserId", userId);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            tickets.Add(new Ticket
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id_Ticket")),
+                                ExhibitionTitle = reader.GetString(reader.GetOrdinal("ExhibitionTitle")),
+                                Date = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                Price = reader.GetDecimal(reader.GetOrdinal("Price"))
+                            });
+                        }
+                    }
+                }
+            }
+
+            return tickets;
+        }
+    }
+}
